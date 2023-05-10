bd85142c0d1f799d63dcd86470e5fc46909bcd3e
robinbaud <72597879+robinbaud@users.noreply.github.com>
2023-05-10 11:24:46 +0200

    authenticate

--- src/index.js
+++ src/index.js
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, { createContext, useContext, useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  redirect,
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/Home";
-import Compte from './pages/Compte';
-import Layout from './pages/Layout';
-import Inscription from './pages/inscription';
-import Connextion from './pages/connextion';
+import Compte from "./pages/Compte";
+import Layout from "./pages/Layout";
+import Inscription from "./pages/inscription";
+import Connextion from "./pages/connextion";
+import Photos from "./pages/Photos";
+import { authContext, AuthProvider } from "./Context/authContext";
+function App() {
+  const { auth } = useContext(authContext);
+  const [token, setToken] = useState("");
 
-export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/compte" element={<Compte />} />
-        <Route path="/inscription" element={<Inscription />} />
-        <Route path="/connextion" element={<Connextion />} />
-
+          <Route index element={<Home />} />
+          <Route path="/compte" element={<Compte />} />
+          <Route path="/inscription" element={<Inscription />} />
+          <Route path="/connextion" element={<Connextion />} />
+          <Route
+            path="/photos"
+            element={auth ? <Photos /> : <Navigate to="/connextion" />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+export default function AppWrapper() {
+  return (
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppWrapper />);
